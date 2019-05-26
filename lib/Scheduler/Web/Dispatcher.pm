3879eb59d20cf19ff820b4625bfacf45bc5ad58c
omokawa <omokawa>
2019-05-26 20:44:30 +0900

    クエリパラメータでTodoの並び順を制御する

--- lib/Scheduler/Web/Dispatcher.pm
+++ lib/Scheduler/Web/Dispatcher.pm
@@ -9,7 +9,10 @@ use Time::Piece;
 any '/' => sub {
     my ($c) = @_;
 
-    my @schedules = $c->db->search('schedules');
+    my $order = $c->req->parameters->{order};
+    my $order_arg = ($order and $order eq 'reverse') ? 'date' : 'date DESC';
+    my @schedules = $c->db->search('schedules', {}, {order_by => $order_arg});
+
     return $c->render('index.tx', { schedules => \@schedules });
 };
 
@@ -26,6 +29,14 @@ post '/post' => sub {
         date  => $date_epoch,
     });
 
+    return $c->redirect('/');
+};
+
+post '/schedules/:id/delete' => sub {
+    my ($c, $args) = @_;
+
+    my $id = $args->{id};
+    $c->db->delete('schedules' => {id => $id});
     return $c->redirect('/');
 };
 
