0edd96b29de59f386f9b1af0890c38d608a525f7
omokawa <omokawa>
2019-05-27 23:08:16 +0900

    ログイン画面およびログイン機能の作成
    ログアウト機能はまだ。

--- lib/Scheduler/Web/Dispatcher.pm
+++ lib/Scheduler/Web/Dispatcher.pm
@@ -9,14 +9,13 @@ use Time::Piece;
 any '/' => sub {
     my ($c) = @_;
 
+    return $c->redirect('/login') unless $c->session->get('user_id');
+
     my $order = $c->req->parameters->{order};
     my $order_arg = ($order and $order eq 'reverse') ? 'date' : 'date DESC';
     my @schedules = $c->db->search('schedules', {}, {order_by => $order_arg});
 
     my $today = Time::Piece->localtime();
-
-    # セッション確立コード
-
 
     return $c->render('index.tx', {
         schedules => \@schedules,
@@ -39,6 +38,35 @@ post '/schedules/:id/delete' => sub {
 
     my $id = $args->{id};
     $c->db->delete('schedules' => {id => $id});
+    return $c->redirect('/');
+};
+
+get '/login' => sub {
+    my ($c) = @_;
+
+    return $c->render('login.tx');
+};
+
+my $users = {
+    1 => { pass => 11111 },
+    2 => { pass => 22222 }
+};
+
+post '/login' => sub {
+    my ($c) = @_;
+
+    my $id = $c->req->parameters->{id};
+    my $password = $c->req->parameters->{password};
+
+    if ($users->{$id}->{pass} eq $password) {
+
+        $c->session->regenerate_id();
+        $c->session->set('user_id' => $id);
+    } else {
+
+        return $c->redirect('/login');
+    }
+
     return $c->redirect('/');
 };
 
